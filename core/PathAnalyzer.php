f68c374567d2da3152a14b2e7e90ebaa6b50697c
lesly1337 <lemmie555@gmail.com>
2018-12-13 19:19:50 +0300

    Complete Path Analyzer

--- core/PathAnalyzer.php
+++ core/PathAnalyzer.php
@@ -4,23 +4,45 @@ class PathAnalyzer {
 
     private $url;
     private $path;
+    private $isValid;
 
     function __construct() {
         $this->url = $this->setUrl();
-        $this->path = $this->setPath();
+
+        if ($this->url !== NULL) {
+            $path = $this->setPath();
+            $this->path = $this->cleanPath($path);
+        }    
+
+        $this->isValid = $this->checkPath();
     }
 
     private function setUrl() {
-        return $_GET['path'];
+        return $_GET["path"];
     }
 
     private function setPath() {
-        return explode('/', $this->url);
+        return explode("/", $this->url);
+    }
+
+    private function cleanPath($path) {
+        $last = count($path) - 1;
+        if ($path[$last] == "") {
+            unset($path[$last]);
+        }
+        return $path;
+    }
+
+    private function checkPath() {
+        return count($this->path) > 2 ? false : true;
     }
 
     public function getPath() {
         return $this->path;
-
     }
    
+    public function getValid() {
+        return $this->isValid;
+    }
+
 }
