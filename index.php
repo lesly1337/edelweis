01d8f764c536b445cd2a6cf93296e056c4988469
lesly1337 <lemmie555@gmail.com>
2019-01-20 16:02:01 +0300

    Change auth data on variables

--- index.php
+++ index.php
@@ -68,8 +68,9 @@ if ( count($uriArray) > 2 ) {
 
 <?php
 
+require "auth/pass.php";
 
-$conn = new PDO("mysql:host=localhost;dbname=edelweis;charset=utf8", "edelweis", "qwerty");
+$conn = new PDO("mysql:host=localhost;dbname=$dbname;charset=utf8", $login, $pass);
 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
