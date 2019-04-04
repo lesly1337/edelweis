d2c3e1190836c1c23a6a51d75d36c533c4352ca0
lesly1337 <lemmie555@gmail.com>
2019-04-04 18:08:44 +0300

    add additional edit info code

--- web/admin/edit_info.php
+++ web/admin/edit_info.php
@@ -1,5 +1,7 @@
 <?php
 
+// ==========================================================
+
 function get_info_site_name($conn) {
     $sth = $conn->prepare("SELECT `site_name` FROM `setting` WHERE `id` = 1");
     $sth->execute();
@@ -25,6 +27,13 @@ function get_info_email_1($conn) {
     return $result ? $result['email_1'] : false;
 }
 
+function get_info_categories($conn) {
+    $sth = $conn->prepare("SELECT `id`, `name` FROM `category` ORDER BY `id`");
+    $sth->execute();
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
 // ==========================================================
 
 function update_info_site_name($conn, $site_name) {
@@ -47,61 +56,87 @@ function update_info_email_1($conn, $email_1) {
     $sth->execute(array(":email_1" => $email_1));
 }
 
+// ==========================================================
+
 if (isset($_POST["info_site_name"]) && $_POST["info_site_name"] !== "") {
     $info_site_name = $_POST["info_site_name"];
     update_info_site_name($conn, $info_site_name);
-
 }
 
 if (isset($_POST["info_slogan"]) && $_POST["info_slogan"] !== "") {
     $info_slogan = $_POST["info_slogan"];
     update_info_slogan($conn, $info_slogan);
-
 }
 
 if (isset($_POST["info_phone_1"]) && $_POST["info_phone_1"] !== "") {
     $info_phone_1 = $_POST["info_phone_1"];
     update_info_phone_1($conn, $info_phone_1);
-
 }
 
 if (isset($_POST["info_email_1"]) && $_POST["info_email_1"] !== "") {
     $info_email_1 = $_POST["info_email_1"];
     update_info_email_1($conn, $info_email_1);
-
 }
 
 // ============================================================
 
-$info_site_name = get_info_site_name($conn);
+$info_site_name  = get_info_site_name($conn);
 
-$info_slogan = get_info_slogan($conn);
+$info_slogan     = get_info_slogan($conn);
 
-$info_phone_1 = get_info_phone_1($conn);
+$info_phone_1    = get_info_phone_1($conn);
 
-$info_email_1 = get_info_email_1($conn);
+$info_email_1    = get_info_email_1($conn);
+
+$info_categories = get_info_categories($conn);
+
+// ==========================================================
 
 ?>
 
-<form method="post">
+<form method="post" id="edit-info">
 
-    <div>
-        Имя сайта: <input type="text" name="info_site_name"> <?php echo $info_site_name; ?>
-    </div>
+    <table>
+<?php
 
-    <div>
-        Слоган: <input type="text" name="info_slogan"> <?php echo $info_slogan; ?>
-    </div>
+    for ($i = 1; $i < count($info_categories); $i++) {
 
-    <div>
-        Телефон: <input type="text" name="info_phone_1"> <?php echo $info_phone_1; ?>
-    </div>
+        echo "<tr>";
+        echo "<td><input type=\"text\" name=\"" . $info_categories[$i]["id"] . "\"></td>";
+        echo "<td>" . $info_categories[$i]["name"] . "</td>";
+        echo "</tr>";
 
-    <div>
-        Email: <input type="text" name="info_email_1"> <?php echo $info_email_1; ?>
-    </div>
+    }
 
-    <input type="submit" value="send">
+?>
+    </table>
 
+    <table>
+        <tr>
+            <th>Имя сайта:</th>
+            <td><input type="text" name="info_site_name"></td>
+            <td><?php echo $info_site_name; ?></td>
+        </tr>
+        <tr>
+            <th>Слоган:</th>
+            <td><input type="text" name="info_slogan"></td>
+            <td><?php echo $info_slogan; ?></td>
+        </tr>
+        <tr>
+            <th>Телефон:</th>
+            <td><input type="text" name="info_phone_1"></td>
+            <td><?php echo $info_phone_1; ?></td>
+        </tr>
+        <tr>
+            <th>Email:</th>
+            <td><input type="text" name="info_email_1"></td>
+            <td><?php echo $info_email_1; ?></td>
+        </tr>
+        <tr>
+            <th></th>
+            <td></td>
+            <td class="right"><input type="submit" value="send"></td>
+        </tr>
+    </table>
 
 </form>
--- web/admin/index.php
+++ web/admin/index.php
@@ -2,6 +2,8 @@
 
 require "check_user.php";
 
+$domain_name = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] === "on" ? "https" : "http") . "://" . $_SERVER["HTTP_HOST"] . "/";
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,7 +14,7 @@ require "check_user.php";
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="<?=$domain_name?>web/admin/style.css">
     <title>Hello, world!</title>
   </head>
   <body>
